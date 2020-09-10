8686e911a6bb32ba771be90058ce720c9c3217c5
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-09-10 09:39:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navigation from "./components/navigation";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import PageRenderer from "./page-renderer";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
